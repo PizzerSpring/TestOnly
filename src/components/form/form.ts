27c95cf45f1649e8abc8d8034de27edd0cea126a
Axendr <vladi-slav96@mail.ru>
2021-05-24 23:56:00 +0700

    POST and GET requests

--- src/components/form/form.ts
+++ src/components/form/form.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { getComponent } from './../../app/js/component';
 import Component, { ComponentProps, getComponents } from '../../app/js/component';
 import Input from '../input/input';
@@ -5,14 +6,30 @@ import FormButton from '../form-button/form-button';
 
 export default class Form extends Component.Default {
     nInputs: Input[];
-    nFormButton: FormButton
+    nFormButton: FormButton;
+    errors: HTMLElement;
+    success: HTMLElement;
     constructor(element: ComponentProps) {
         super(element);
 
         this.nInputs = getComponents('input', this.nRoot).map(component => new Input(component, this.upDateButton));
         this.nFormButton = new FormButton(getComponent('form-button', this.nRoot));
 
-        this.nRoot.addEventListener('submit', this.onSubmit)
+        this.errors = this.getElement('errors');
+        this.success = this.getElement('success');
+
+        this.nRoot.addEventListener('submit', this.onSubmit);
+        
+
+        axios.get('http://dev.studio-mind.ru/api/form')
+        .then(result => {
+            for(let i = 0; i < result.data.list.length; i++) {
+                console.log(result.data.list[i].nameId);
+            }
+        })
+        .catch(error => console.log(error));
+
+        
     }
 
     upDateButton = () => {
@@ -21,13 +38,18 @@ export default class Form extends Component.Default {
             return true;
         });
 
-
         this.nFormButton.setDisabled(!state);
         
-        
-    }
+    };
 
-    onSubmit = (e: Event) => {
+
+     
+
+    
+
+    
+
+    onSubmit = async (e: Event) => {
         e.preventDefault();
 
         let data: any = {};
@@ -37,9 +59,30 @@ export default class Form extends Component.Default {
             data[item.name] = item.value;
         });
 
-        console.log(data);
+        const obj = {
+            ...data,
+            nameId: "vladislav2"
+        };
+        
+
+        try {
+            const response = await axios.post('http://dev.studio-mind.ru/api/form', obj);
+            const div = document.createElement('div');
+            div.className = `${this.nRootName}__success-item`;
+            div.innerHTML = 'Форма успешно отправлена';
+
+            this.success.appendChild(div);
+        } catch(e) {
+            const div = document.createElement('div');
+            div.className = `${this.nRootName}__errors-item`;
+            div.innerHTML = e.message;
+
+            this.errors.appendChild(div);
+        }
 
     }
+
+    
 
     destroy = () => {
         // Destroy functions
